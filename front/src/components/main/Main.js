31a59f3b8082695cb6526de8256e6fc575915ff7
bautigonzalez <batigonza@gmail.com>
2020-08-24 16:58:04 -0300

    front

---
+++ front/src/components/main/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+
+export default () => {
+    return (
+      <div id="main">
+          main
+      </div>
+    );
+  };
